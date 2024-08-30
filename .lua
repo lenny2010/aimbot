4d3024ad501d3fb7c38fba0fb4fd69dfbf53bf89
lenny2010 <143286520+lenny2010@users.noreply.github.com>
2024-08-30 19:46:46 +0200

    Update .lua

--- .lua
+++ .lua
@@ -7,14 +7,14 @@ local LocalPlayer = Players.LocalPlayer
 local Holding = false
 
 _G.AimbotEnabled = true
-_G.TeamCheck = false -- If set to true then the script would only lock your aim at enemy team members.
+_G.TeamCheck = true -- If set to true then the script would only lock your aim at enemy team members.
 _G.AimPart = "Head" -- Where the aimbot script would lock at.
-_G.Sensitivity = 0 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
+_G.Sensitivity = 1 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
 
 _G.CircleSides = 64 -- How many sides the FOV circle would have.
 _G.CircleColor = Color3.fromRGB(255, 255, 255) -- (RGB) Color that the FOV circle would appear as.
 _G.CircleTransparency = 0.7 -- Transparency of the circle.
-_G.CircleRadius = 35 -- The radius of the circle / FOV.
+_G.CircleRadius = 50 -- The radius of the circle / FOV.
 _G.CircleFilled = false -- Determines whether or not the circle is filled.
 _G.CircleVisible = true -- Determines whether or not the circle is visible.
 _G.CircleThickness = 0 -- The thickness of the circle.
